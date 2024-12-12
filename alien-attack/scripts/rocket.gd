7f6357dcf07a39a1c26806fdaa09c50829fc7bca
epalonen <esa-pekka.palonen@tuni.fi>
2024-12-12 17:47:30 +0200

    When rocket exits the screen it is deleted

--- alien-attack/scripts/rocket.gd
+++ alien-attack/scripts/rocket.gd
@@ -1,10 +1,18 @@
 extends Area2D
 
 @export var speed = 300
+@onready var visible_notifier = get_node("VisibleNotifier") #same as $VisibleNotifier
 
+func _ready():
+	visible_notifier.connect("screen_exited", _on_screen_exited)
 
 func _physics_process(delta):
 	rocket_movement(delta)
 	
 func rocket_movement(delta):
 	global_position.x += speed * delta
+
+func _on_screen_exited():
+	print("Rocket has left the building!")
+	queue_free()
+	
