ee7e3c411d80b06568d420789fccc29623ac9224
epalonen <esa-pekka.palonen@tuni.fi>
2024-12-12 17:10:53 +0200

    instantiate rocket from player

--- alien-attack/scripts/player.gd
+++ alien-attack/scripts/player.gd
@@ -2,10 +2,16 @@ extends CharacterBody2D
 
 var speed = 200
 
+var rocket_scene = preload("res://scenes/rocket.tscn")
+
 func _physics_process(delta):
-	player_movement(delta)
-	
-func player_movement(delta):
+	player_movement()
+
+func  _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+		
+func player_movement():
 	velocity = Vector2(0,0)
 	if Input.is_action_pressed("move_right"):
 		velocity.x = speed
@@ -15,12 +21,22 @@ func player_movement(delta):
 		velocity.y = -speed
 	if Input.is_action_pressed("move_down"):
 		velocity.y = speed
-	
 		
 	move_and_slide()
 	
 	var screen_size = get_viewport_rect().size
-	#
+		
+	global_position = global_position.clamp(Vector2(0,0), screen_size)
+
+
+func shoot():
+	print("shoot")
+	var rocket_instance = rocket_scene.instantiate()
+	add_child(rocket_instance)
+	rocket_instance.global_position.x += 75
+	
+	
+#
 	#if global_position.x < 0:
 		#global_position.x = 0
 	#if global_position.x > screen_size.x:
@@ -34,8 +50,3 @@ func player_movement(delta):
 	
 	# global_position.x =clampf(global_position.x, 0, screen_size.x)
 	# global_position.y =clampf(global_position.y, 0, screen_size.y)
-	
-	global_position = global_position.clamp(Vector2(0,0), screen_size)
-
-
-	
